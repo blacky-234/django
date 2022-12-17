d1f51a970c0ef792996a5ec3ae30e271e5a29855
karuppsamyradan <karuppsamyradan@gmail.com>
2022-12-17 12:59:51 +0530

    first

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "users"
+
+urlpatterns = [ 
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="create"),
+    path("signup/newsignup/", views.newsignup, name="signup"),
+    path("login/alogin/", views.alogin, name="verify"),
+    # path("logout/", views.logout_view, name="logout"),
+]
---
+++ users/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from . models import User
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'auth/_login.html')
+
+
+def signup(request):
+    return render(request, 'auth/_signup.html')
+
+
+def newsignup(request):
+    if request.method == "POST":
+        name = request.POST['username']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username=name, email=email, password=password, phone=phone)
+        user.save()
+        return login(request)
+    else:
+        return redirect('signup')
+
+
+def alogin(request):
+    if request.method == "POST":
+        lname = request.POST['email_address']
+        passwd = request.POST["password"]
+        user = authenticate(username=lname, password=passwd)
+        if user.is_authenticated:               
+            return render(request, 'album/album.html') 
+        else:
+            # TODO: check login header connections
+            return login(request)
+# @logout
+# def logout_view(request):
+#     logout(request)
+#     return HttpResponse("<h1> logout successfully </h1>")
+    
